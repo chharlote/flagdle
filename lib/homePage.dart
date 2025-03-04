f4f32bf31335b569a172764f01aee7ee2b73dc80
Noa66 <noadeletangdevin@gmail.com>
2025-03-04 16:29:02 +0100

    Création de la branche Home + création de la page

---
+++ lib/homePage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Home extends StatelessWidget{
+  const Home({required});
+
+  @override
+  Widget build(BuildContext){
+    return Container();
+  }
+}

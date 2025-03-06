1476d70f9a5fce0c5eebccec9fa0a53bb21082e0
Charlotte Germe <charlotte.germe16@gmail.com>
2025-03-06 16:25:39 +0100

    feat: store qui gère le score du jeu

--- lib/AppConst.dart
+++ lib/AppConst.dart
@@ -1,3 +1,8 @@
 class AppConst {
   static String API_KEY = "2296|znaWnbESD2Wis60EcKJStclNFgvbhpBrtrv2ctDO";
+  static int scoreDefault = 0;
+  static int scoreMin = 0;
+  static int scoreMax = 20;
+  static const scoreKey = "prefs_score_key";
+
 }

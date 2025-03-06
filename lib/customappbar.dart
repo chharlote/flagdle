a194bbbc23f0e12b002d370ad1ff38e24c31b336
Matthieu <matthieu1.pinceel@gmail.com>
2025-03-06 16:51:56 +0100

    création page de jeu (a finir)

---
+++ lib/customappbar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: const Text(
+        'Flagdle',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.pinkAccent,
+          fontSize: 47,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+
+      backgroundColor: Colors.black,
+    );
+
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
